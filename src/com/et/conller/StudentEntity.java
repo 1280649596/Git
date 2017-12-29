2df965671ef73c4c79a0b568da502301f1ae21e4
Administrator <Administrator@PC-20171220XSLQ>
2017-12-29 10:10:41 +0800

    dsfgedv

---
+++ src/com/et/conller/StudentEntity.java
@@ -0,0 +1,46 @@
+package com.et.conller;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class StudentEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Column
+	private String name;
+	@Column
+	private String sex;
+	@Column
+	private int age;
+	
+}
